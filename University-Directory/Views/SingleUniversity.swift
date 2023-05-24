686d65a88beb6ec92f18de2694cf150a6f39ee12
mirzashowvik <showvikmirza@gmail.com>
2023-05-24 17:46:36 +0600

    fix university page

---
+++ University-Directory/Views/SingleUniversity.swift
@@ -0,0 +1,20 @@
+//
+//  SingleUniversity.swift
+//  University-Directory
+//
+//  Created by Mirza  on 24/5/23.
+//
+
+import SwiftUI
+
+struct SingleUniversity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SingleUniversity_Previews: PreviewProvider {
+    static var previews: some View {
+        SingleUniversity()
+    }
+}
